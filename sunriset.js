7c7aacf639fd7de1c60e6233875459f43d2a1b6c
Neil de Carteret <n3dst4@gmail.com>
2009-10-14 19:11:40 +0100

    final (?) round of comment tidying.

--- sunriset.js
+++ sunriset.js
@@ -224,52 +224,52 @@ function astronomical_twilight(year, month, day, lon, lat) {
  * `tset`: time of sunset (or end of period) as floating point hour value.
  */
 function __sunriset__(year, month, day, lon, lat, altit, upper_limb) {
-    var  d,  /* Days since 2000 Jan 0.0 (negative before) */
-    sr,         /* Solar distance, astronomical units */
-    sRA,        /* Sun's Right Ascension */
-    sdec,       /* Sun's declination */
-    sradius,    /* Sun's apparent radius */
-    t,          /* Diurnal arc */
-    tsouth,     /* Time when Sun is at south */
-    sidtime;    /* Local sidereal time */
+    var  d,     // Days since 2000 Jan 0.0 (negative before)
+    sr,         // Solar distance, astronomical units
+    sRA,        // Sun's Right Ascension
+    sdec,       // Sun's declination
+    sradius,    // Sun's apparent radius
+    t,          // Diurnal arc
+    tsouth,     // Time when Sun is at south
+    sidtime;    // Local sidereal time
     var ret = {};
-    var rc = 0; /* Return code from function - usually 0 */
+    var rc = 0; // Return code from function - usually 0
 
-    /* Compute d of 12h local mean solar time */
+    // Compute d of 12h local mean solar time
     d = days_since_2000_Jan_0(year,month,day) + 0.5 - lon/360.0;
 
-    /* Compute local sideral time of this moment */
+    // Compute local sideral time of this moment
     sidtime = revolution( GMST0(d) + 180.0 + lon );
 
-    /* Compute Sun's RA + Decl at this moment */
+    // Compute Sun's RA + Decl at this moment
     var my_sun_ra_dec = sun_RA_dec(d);
     sRA = my_sun_ra_dec.RA;
     sdec = my_sun_ra_dec.dec;
     sr = my_sun_ra_dec.r;
 
-    /* Compute time when Sun is at south - in hours UT */
+    // Compute time when Sun is at south - in hours UT
     tsouth = 12.0 - rev180(sidtime - sRA)/15.0;
 
-    /* Compute the Sun's apparent radius, degrees */
+    // Compute the Sun's apparent radius, degrees
     sradius = 0.2666 / sr;
 
-    /* Do correction to upper limb, if necessary */
+    // Do correction to upper limb, if necessary
     if ( upper_limb )
         altit -= sradius;
 
-    /* Compute the diurnal arc that the Sun traverses to reach */
-    /* the specified altitide altit: */
+    // Compute the diurnal arc that the Sun traverses to reach
+    // the specified altitide altit:
     var cost;
     cost = ( sind(altit) - sind(lat) * sind(sdec) ) /
           ( cosd(lat) * cosd(sdec) );
     if ( cost >= 1.0 )
-          rc = -1, t = 0.0;       /* Sun always below altit */
+          rc = -1, t = 0.0;       // Sun always below altit
     else if ( cost <= -1.0 )
-          rc = +1, t = 12.0;      /* Sun always above altit */
+          rc = +1, t = 12.0;      // Sun always above altit
     else
-          t = acosd(cost)/15.0;   /* The diurnal arc, hours */
+          t = acosd(cost)/15.0;   // The diurnal arc, hours
 
-    /* Store rise and set times - in hours UT */
+    // Store rise and set times - in hours UT
     ret.trise = tsouth - t;
     ret.tset  = tsouth + t;
     ret.rc = rc;
@@ -299,109 +299,132 @@ function __sunriset__(year, month, day, lon, lat, altit, upper_limb) {
  * The length of the day in floating-point hours (e.g. 12h 30m is 12.5)
  */
 function __daylen__(year, month, day, lon, lat, altit, upper_limb) {
-    var  d,  /* Days since 2000 Jan 0.0 (negative before) */
-    obl_ecl,    /* Obliquity (inclination) of Earth's axis */
-    sr,         /* Solar distance, astronomical units */
-    slon,       /* True solar longitude */
-    sin_sdecl,  /* Sine of Sun's declination */
-    cos_sdecl,  /* Cosine of Sun's declination */
-    sradius,    /* Sun's apparent radius */
-    t;          /* Diurnal arc */
+    var  d,     // Days since 2000 Jan 0.0 (negative before)
+    obl_ecl,    // Obliquity (inclination) of Earth's axis
+    sr,         // Solar distance, astronomical units
+    slon,       // True solar longitude
+    sin_sdecl,  // Sine of Sun's declination
+    cos_sdecl,  // Cosine of Sun's declination
+    sradius,    // Sun's apparent radius
+    t;          // Diurnal arc
 
-    /* Compute d of 12h local mean solar time */
+    // Compute d of 12h local mean solar time
     d = days_since_2000_Jan_0(year,month,day) + 0.5 - lon/360.0;
 
-    /* Compute obliquity of ecliptic (inclination of Earth's axis) */
+    // Compute obliquity of ecliptic (inclination of Earth's axis)
     obl_ecl = 23.4393 - 3.563E-7 * d;
 
-    /* Compute Sun's position */
+    // Compute Sun's position
     var mysunpos = sunpos(d);
     lon = mysunpos.lon;
     sr = mysunpos.r;
 
-    /* Compute sine and cosine of Sun's declination */
+    // Compute sine and cosine of Sun's declination
     sin_sdecl = sind(obl_ecl) * sind(slon);
     cos_sdecl = sqrt( 1.0 - sin_sdecl * sin_sdecl );
 
-    /* Compute the Sun's apparent radius, degrees */
+    // Compute the Sun's apparent radius, degrees
     sradius = 0.2666 / sr;
 
-    /* Do correction to upper limb, if necessary */
+    // Do correction to upper limb, if necessary
     if ( upper_limb )
         altit -= sradius;
 
-    /* Compute the diurnal arc that the Sun traverses to reach */
-    /* the specified altitide altit: */
+    // Compute the diurnal arc that the Sun traverses to reach
+    // the specified altitide altit: */
     var cost;
     cost = ( sind(altit) - sind(lat) * sin_sdecl ) /
           ( cosd(lat) * cos_sdecl );
     if ( cost >= 1.0 )
-          t = 0.0;                      /* Sun always below altit */
+          t = 0.0;                      // Sun always below altit
     else if ( cost <= -1.0 )
-          t = 24.0;                     /* Sun always above altit */
-    else  t = (2.0/15.0) * acosd(cost); /* The diurnal arc, hours */
+          t = 24.0;                     // Sun always above altit
+    else  t = (2.0/15.0) * acosd(cost); // The diurnal arc, hours
 
     return t;
 }
 
 
 /*
- * This function computes the Sun's position at any instant
+ * Computes the Sun's position at any instant
+ *
+ * Computes the Sun's ecliptic longitude and distance
+ * at an instant given in d, number of days since
+ * 2000 Jan 0.0.  The Sun's ecliptic latitude is not
+ * computed, since it's always very near 0.
+ *
+ * ### Arguments
+ * d: number of days since 2000 Jan 0.0.
+ *
+ * ### Returns
+ * An object with two properties:
+ * lon: Sun's ecliptic longitude
+ * r: Solar distance
  */
 function sunpos(d) {
-    /******************************************************/
-    /* Computes the Sun's ecliptic longitude and distance */
-    /* at an instant given in d, number of days since     */
-    /* 2000 Jan 0.0.  The Sun's ecliptic latitude is not  */
-    /* computed, since it's always very near 0.           */
-    /******************************************************/
-    var M,         /* Mean anomaly of the Sun */
-        w,         /* Mean longitude of perihelion */
-                    /* Note: Sun's mean longitude = M + w */
-        e,         /* Eccentricity of Earth's orbit */
-        E,         /* Eccentric anomaly */
-        x, y,      /* x, y coordinates in orbit */
-        v;         /* True anomaly */
+    var M,         // Mean anomaly of the Sun
+        w,         // Mean longitude of perihelion
+                   // Note: Sun's mean longitude = M + w
+        e,         // Eccentricity of Earth's orbit
+        E,         // Eccentric anomaly
+        x, y,      // x, y coordinates in orbit
+        v;         // True anomaly
     var pos = {};
 
-    /* Compute mean elements */
+    // Compute mean elements
     M = revolution( 356.0470 + 0.9856002585 * d );
     w = 282.9404 + 4.70935E-5 * d;
     e = 0.016709 - 1.151E-9 * d;
 
-    /* Compute true longitude and radius vector */
+    // Compute true longitude and radius vector
     E = M + e * RADEG * sind(M) * ( 1.0 + e * cosd(M) );
         x = cosd(E) - e;
     y = Math.sqrt( 1.0 - e*e ) * sind(E);
-    pos.r = Math.sqrt( x*x + y*y );              /* Solar distance */
-    v = atan2d( y, x );                  /* True anomaly */
-    pos.lon = v + w;                        /* True solar longitude */
+    pos.r = Math.sqrt( x*x + y*y ); // Solar distance
+    v = atan2d( y, x );             // True anomaly
+    pos.lon = v + w;                // True solar longitude
     if ( pos.lon >= 360.0 )
-        pos.lon -= 360.0;                   /* Make it 0..360 degrees */
+        pos.lon -= 360.0;           // Make it 0..360 degrees
     return pos;
 }
 
+
+/*
+ * Compute sun's right ascension and declination.
+ *
+ * ### Arguments
+ * d: number of days since 2000 Jan 0.0.
+ *
+ * ### Returns
+ * An object with two properties:
+ * RA: Sun's right ascension.
+ * dec: Sun's declination
+ *
+ * ### Notes
+ * This function was undocumented in SUNRISET.C - I've filled it in with what I
+ * can guess :)
+ */
 function sun_RA_dec (d) {
       var lon, obl_ecl, x, y, z, r;
       var ret = {};
 
-      /* Compute Sun's ecliptical coordinates */
+      // Compute Sun's ecliptical coordinates
       mysunpos = sunpos(d);
       lon = mysunpos.lon;
       r = ret.r = mysunpos.r;
 
-      /* Compute ecliptic rectangular coordinates (z=0) */
+      // Compute ecliptic rectangular coordinates (z=0)
       x = r * cosd(lon);
       y = r * sind(lon);
 
-      /* Compute obliquity of ecliptic (inclination of Earth's axis) */
+      // Compute obliquity of ecliptic (inclination of Earth's axis)
       obl_ecl = 23.4393 - 3.563E-7 * d;
 
-      /* Convert to equatorial rectangular coordinates - x is uchanged */
+      // Convert to equatorial rectangular coordinates - x is uchanged
       z = y * sind(obl_ecl);
       y = y * cosd(obl_ecl);
 
-      /* Convert to spherical coordinates */
+      // Convert to spherical coordinates
       ret.RA = atan2d( y, x );
       ret.dec = atan2d( z, Math.sqrt(x*x + y*y) );
 
